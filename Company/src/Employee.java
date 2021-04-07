6412e724500c418f4d74e39c3ca36da9b22450ab
sysadmin <sysadmin@bcalab1-m13>
2021-04-07 12:21:16 +0530

    Method Added

--- Company/src/Employee.java
+++ Company/src/Employee.java
@@ -8,9 +8,15 @@ public class Employee {
 		this.name = name;
 		this.dept = dept;
 	}
+	
+	void display()
+	{
+	System.out.println("Name is" + name);
+	System.out.println("Dept is" + dept);
+	}
 	public static void main(String[] args)
 	{
-		
+	
 		//TODO Auto-generated method stub
 	}
 }
