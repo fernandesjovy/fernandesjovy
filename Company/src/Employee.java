45526bd846ca5089170b61493ca6df45e8e570fe
sysadmin <sysadmin@bcalab1-m13>
2021-04-07 11:55:36 +0530

    Project Pushed

---
+++ Company/src/Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee {
+
+	String name;
+	String dept;
+	Employee(String name, String dept)
+	{
+		this.name = name;
+		this.dept = dept;
+	}
+	public static void main(String[] args)
+	{
+		
+		//TODO Auto-generated method stub
+	}
+}
